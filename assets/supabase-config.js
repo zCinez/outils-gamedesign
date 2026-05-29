3de0c6d4cdb2e92725690dfdab4ff3a2bf1fef30
zCinez <samgiant1007@gmail.com>
2026-05-29 16:34:47 +0200

    db

--- assets/supabase-config.js
+++ assets/supabase-config.js
@@ -1,5 +1,5 @@
 window.NEODIUM_SUPABASE_CONFIG = Object.freeze({
-  url: "https://koslfxbmcnbgctcyshmh.supabase.co",
+  url: "https://koslfxkbmcnbgtcyshmh.supabase.co",
   anonKey: "sb_publishable_ZznjWeKWsCpq-Aws7fbwxQ_1KWzlJAD",
   storageTable: "neodium_user_storage",
   syncKeyPrefixes: ["neodium_", "neodium-", "statgm-"],
